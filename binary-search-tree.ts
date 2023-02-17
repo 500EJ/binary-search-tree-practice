9575ccef263267925725c5f8304d4bf101b2bfca
Ethan Julius <56324629+500EJ@users.noreply.github.com>
2023-02-17 16:06:36 -0500

    Initial commit

---
+++ binary-search-tree.ts
@@ -0,0 +1,48 @@
+// Do not change this
+class TreeNode {
+  val: number;
+  left: TreeNode | null;
+  right: TreeNode | null;
+
+  constructor(val: number) {
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+export class BinarySearchTree {
+  constructor() {
+    // Your code here
+  }
+
+  insert(val, currentNode = this.root) {
+    // Your code here
+  }
+
+  search(val) {
+    // Your code here
+  }
+
+  preOrderTraversal(currentNode = this.root) {
+    // Your code here
+  }
+
+  inOrderTraversal(currentNode = this.root) {
+    // Your code here
+  }
+
+  postOrderTraversal(currentNode = this.root) {
+    // Your code here
+  }
+
+  // Breadth First Traversal - Iterative
+  breadthFirstTraversal() {
+    // your code here
+  }
+
+  // Depth First Traversal - Iterative
+  depthFirstTraversal() {
+    // your code here
+  }
+}
